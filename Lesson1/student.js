18188a752771506060717f2e4ddcbec7f83fab05
SheikhAmin <sheikhamin.cs@gmail.com>
2022-05-21 13:24:52 +0600

    My First Commit

---
+++ Lesson1/student.js
@@ -0,0 +1,19 @@
+const getAge = () => {
+    return "25";
+}
+
+const getName = () => {
+    return "Anisul Islam";
+}
+
+const cgpa = 3.92;
+
+//exports.getName = getName;
+//exports.getAge = getAge;
+//exports.result = cgpa;
+
+module.exports = {
+    getName,
+    getAge,
+    cgpa
+}
